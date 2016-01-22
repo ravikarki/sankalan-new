e50d977b36b53de46900b6eef4c916157a69ddc1
ravikarki <ravikarki.7@gmail.com>
2016-01-22 19:39:59 +0530

    Code to live
    index.html is main file

---
+++ js/pop.js
@@ -0,0 +1,429 @@
+/*
+* Modalzinha.js
+*
+* by Fernando Soares (dotfernando@gmail.com)
+* github.com/ferspeak/modalzinha-js
+*
+*/
+
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_algo"]'),
+      modalClose = document.querySelector('[data-modal="close_algo"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_algo"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_bs"]'),
+      modalClose = document.querySelector('[data-modal="close_bs"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_bs"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_ch"]'),
+      modalClose = document.querySelector('[data-modal="close_ch"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_ch"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_ct"]'),
+      modalClose = document.querySelector('[data-modal="close_ct"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_ct"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_d++"]'),
+      modalClose = document.querySelector('[data-modal="close_d++"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_d++"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_jj"]'),
+      modalClose = document.querySelector('[data-modal="close_jj"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_jj"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_ma"]'),
+      modalClose = document.querySelector('[data-modal="close_ma"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_ma"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_mm"]'),
+      modalClose = document.querySelector('[data-modal="close_mm"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_mm"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_sb"]'),
+      modalClose = document.querySelector('[data-modal="close_sb"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_sb"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_sw"]'),
+      modalClose = document.querySelector('[data-modal="close_sw"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_sw"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_tp"]'),
+      modalClose = document.querySelector('[data-modal="close_tp"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_tp"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_ts"]'),
+      modalClose = document.querySelector('[data-modal="close_ts"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_ts"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_lan"]'),
+      modalClose = document.querySelector('[data-modal="close_lan"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_lan"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_jam"]'),
+      modalClose = document.querySelector('[data-modal="close_jam"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_jam"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(e) {
+            e.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(e) {
+              e.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+
+Modal.init();
+var Modal = (function() {
+
+  var modalOpen = document.querySelector('[data-modal="open_turn"]'),
+      modalClose = document.querySelector('[data-modal="close_turn"]'),
+      modalWrapper = document.querySelector('[data-modal="wrapper_turn"]');
+
+      return {
+        init: function() {
+          this.abrir();
+          this.fechar();
+        },
+
+        abrir: function() {
+          modalOpen.onclick = function(t) {
+            t.preventDefault;
+              modalWrapper.classList.add("modal-opened");
+          }
+        },
+
+        fechar: function() {
+            modalClose.onclick = function(t) {
+              t.preventDefault;
+              modalWrapper.classList.remove("modal-opened");
+            }
+        }
+      }
+}());
+Modal.init();
